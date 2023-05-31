fca910d05e8f9bf2d6200f9aee0bf52f53cc73ff
eric <eric.komachi@gmail.com>
2023-05-31 00:37:44 -0400

    fixed formatting

--- src/components/helper/alerts/SnackbarAlert.js
+++ src/components/helper/alerts/SnackbarAlert.js
@@ -1,10 +1,7 @@
 import MuiAlert from '@mui/material/Alert';
 import { forwardRef } from 'react';
 
-const SnackbarAlert = forwardRef(function Alert(
-  props,
-  ref,
-) {
+const SnackbarAlert = forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
