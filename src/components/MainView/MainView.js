c7fa16284b41c14783e1ac5c6d3d819bbac7bbf8
eric <eric.komachi@gmail.com>
2023-05-30 20:25:48 -0400

    updated MainView to use routes

--- src/components/MainView/MainView.js
+++ src/components/MainView/MainView.js
@@ -1,22 +1,13 @@
 import './MainView.css';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import CreateUser from '@components/CreateUser/CreateUser';
 
 function MainView() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<CreateUser />} />
+    </Routes>
   );
 }
 
