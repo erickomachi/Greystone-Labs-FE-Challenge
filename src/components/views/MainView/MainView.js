487340faa32d34057b650ac1d04f50ceea778b37
eric <eric.komachi@gmail.com>
2023-06-01 16:34:18 -0400

    removed and updated boilerplate css from MainView

--- src/components/views/MainView/MainView.js
+++ src/components/views/MainView/MainView.js
@@ -1,19 +1,32 @@
 import './MainView.css';
-import { Route } from 'react-router-dom';
-import { Routes } from 'react-router-dom';
+import { Grid, Container } from "@mui/material";
+
+import { Route, Routes } from 'react-router-dom';
 import CreateUser from '@components/views/CreateUser/CreateUser';
 import CreateLoan from '@components/views/CreateLoan/CreateLoan';
 import GetAllLoans from '@components/views//GetAllLoans/GetAllLoans';
 import ShareLoan from '@components/views/ShareLoan/ShareLoan';
+import NavigationPane from '@components/views/NavigationPane/NavigationPane';
+import Home from '@components/views/Home/Home';
 
 function MainView() {
   return (
-    <Routes>
-      <Route path="/" element={<CreateUser />} />
-      <Route path="/create-loan" element={<CreateLoan />} />
-      <Route path="/get-all-loans" element={<GetAllLoans />} />
-      <Route path="/share-loan" element={<ShareLoan />} />
-    </Routes>
+    <Grid container direction={"column"} justifyContent="space-evenly" alignItems={"stretch"} >
+      <Grid item xs={2}>
+        <NavigationPane />
+      </Grid>
+      <Grid item xs={10}>
+        <Container disableGutters={false} >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create-user" element={<CreateUser />} />
+            <Route path="/create-loan" element={<CreateLoan />} />
+            <Route path="/get-all-loans" element={<GetAllLoans />} />
+            <Route path="/share-loan" element={<ShareLoan />} />
+          </Routes>
+        </Container>
+      </Grid>
+    </Grid>
   );
 }
 
