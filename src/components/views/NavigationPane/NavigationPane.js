d9f76a83b998ca2e2010d91ec0289f81ffb0a678
eric <eric.komachi@gmail.com>
2023-06-01 22:21:51 -0400

    added NavigationPane.test

--- src/components/views/NavigationPane/NavigationPane.js
+++ src/components/views/NavigationPane/NavigationPane.js
@@ -1,4 +1,4 @@
-import { React } from "react"
+import React from "react"
 import styles from "./NavigationPane.module.css"
 import { Link } from "react-router-dom";
 import { Grid } from "@mui/material";
